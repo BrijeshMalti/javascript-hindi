9e4f03f81df7f3cd3d751d077cb2ff080bcf276f
BrijeshMalti <brijeshkushwaha2003@outlook.com>
2024-05-31 09:15:48 +0000

    conversion of data type

---
+++ javascript_series/03_conversionOperation.js
@@ -0,0 +1,27 @@
+// let score = 33
+let score = 'Brijesh'
+ 
+// Ther are tow type of print in variable
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number (score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => number
+//"333abce"=> NaN
+// 1  => true/ 0 false
+
+let isLoggedIn = "Brijesh"
+let boolenIsLoggedIn = Boolean (isLoggedIn);
+
+// 1 => ture; 0=> false
+// "" => false
+// "Brijesh" => true
+
+let someNumber = 33
+let stringNumber = String (someNumber)
+
+console.log(someNumber);
+console.log( typeof someNumber);
