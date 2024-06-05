1a57e12465ced1adc2592f8d3b4a764e781f2a05
BrijeshMalti <brijeshkushwaha2003@outlook.com>
2024-06-05 12:45:53 +0000

    string variable

---
+++ javascript_series/04_string.js
@@ -0,0 +1,21 @@
+const name = 'brijesh'
+const repoCount =50
+
+// console.log(name + repoCount + "value")
+
+// console.log(`Hello my name is ${name} and my repo count ${repoCount}`);
+
+const gameName = new String ('Brijesh')
+
+
+// console.log(gameName[0])
+// console.log(gameName.length)
+// console.log(gameName.__proto__);
+
+// console.log(gameName.toUpperCase())
+// console.log(gameName.charAt(4))
+// console.log(gameName.indexOf('h'))
+
+const newString = gameName.substring(0, 4)
+
+console.log(newString);
